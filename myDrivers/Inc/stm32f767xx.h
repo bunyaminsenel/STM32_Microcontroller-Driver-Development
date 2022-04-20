6267fb993fc4972984f4f261a3189e92fe53bbfd
Bünyamin Şenel <35276132+bunyaminsenel@users.noreply.github.com>
2022-04-20 15:44:20 +0300

    APB2 and AHB1 Peripherals Base Address

--- myDrivers/Inc/stm32f767xx.h
+++ myDrivers/Inc/stm32f767xx.h
@@ -45,11 +45,38 @@
 #define USART3_BASE_ADRR        (APB1_BASE_ADDR + 0x4800UL)         /* USART2 Base Address */
 #define UART4_BASE_ADRR         (APB1_BASE_ADDR + 0x4C00UL)         /* UART4 Base Address  */
 #define UART5_BASE_ADRR         (APB1_BASE_ADDR + 0x5000UL)         /* UART5 Base Address  */ 
-
+    
 #define I2C1_BASE_ADDR          (APB1_BASE_ADDR + 0x5400UL)         /* I2C1 Base Address  */
 #define I2C2_BASE_ADDR          (APB1_BASE_ADDR + 0x5800UL)         /* I2C2 Base Address  */
 #define I2C3_BASE_ADDR          (APB1_BASE_ADDR + 0x5C00UL)         /* I2C3 Base Address  */
 #define I2C4_BASE_ADDR          (APB1_BASE_ADDR + 0x6000UL)         /* I2C4 Base Address  */
 
+/*
+*  APB2 Peripherals Base Address
+*/
+
+#define TIM1_BASE_ADDR          (APB2_BASE_ADDR + 0x0000UL)         /* TIM1 Base Address  */
+#define TIM8_BASE_ADDR          (APB2_BASE_ADDR + 0x0400UL)         /* TIM2 Base Address  */
+
+#define USART1_BASE_ADDR        (APB2_BASE_ADDR + 0x1000UL)         /* USART1 Base Address */
+#define USART6_BASE_ADDR        (APB2_BASE_ADDR + 0x1400UL)         /* USART6 Base Address */
+
+#define SPI1_BASE_ADDR          (APB2_BASE_ADDR + 0x3000UL)         /* SPI1 Base Address   */    
+#define SPI2_BASE_ADDR          (APB2_BASE_ADDR + 0x3400UL)         /* SPI2 Base Address   */
+
+#define SYSCFG_BASE_ADRR        (APB2_BASE_ADDR + 0x3800UL)         /* SYSCFG Base Address */
+#define EXTI_BASE_ADDR          (APB2_BASE_ADDR + 0x3C00UL)         /* EXTI Base Address   */
+
+/*
+*  AHB1 Peripherals Base Address
+*/
+
+#define GPIOA_BASE_ADDR         (AHB1_BASE_ADDR + 0x0000UL)          /* GPIOA Base Address  */
+#define GPIOB_BASE_ADDR         (AHB1_BASE_ADDR + 0x0400UL)          /* GPIOB Base Address  */
+#define GPIOC_BASE_ADDR         (AHB1_BASE_ADDR + 0x0800UL)          /* GPIOC Base Address  */
+#define GPIOD_BASE_ADDR         (AHB1_BASE_ADDR + 0x0C00UL)          /* GPIOD Base Address  */
+#define GPIOE_BASE_ADDR         (AHB1_BASE_ADDR + 0x1000UL)          /* GPIOE Base Address  */
+
+#define RCC_BASE_ADDR           (AHB1_BASE_ADDR + 0x3800UL)           /* RCC Base Address  */
 
 #endif /* INC_STM32F767XX_H_ */
